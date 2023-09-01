21a1ccfb153fab6b14588be7cc9ea99e0a48a40e
Ajaydev8 <131373049+Ajaydev8@users.noreply.github.com>
2023-09-01 14:07:37 +0530

    Learned features: refactor, split screen, TODO
    Here I learned some features of the PyCharm again, What I learned here is the PEP 8 style of coding which is most known style for writing The CODE. I also learned the features like How to watch the split screen, How to refactor multiple function, understood the structure. With this I completed the basic journey of the PyCharm.

---
+++ Pycharm-practice/data.py
@@ -0,0 +1,10 @@
+contacts = {
+    "James": {
+        "phone_number": 123456789,
+        "email": "james@gmail.com"
+    },
+    "Angela": {
+        "phone_number": 987654321,
+        "email": "angela@gmail.com"
+    }
+}
---
+++ Pycharm-practice/main.py
@@ -0,0 +1,37 @@
+#  Pycharm comes with the spell check.
+
+# It comes with the more space.
+# You can split the screen and see the data side by side.
+
+from data import contacts
+
+angela_email = contacts["Angela"]["phone_number"]
+
+print(angela_email)
+
+# Pycharm has built-in linter.
+# Python style follows the PEP 8 guide for writing code.
+
+
+def add(n1, n2):
+    total = n1 + n2
+    return total
+
+
+add(4, 5)
+
+# We can also go to local history
+# by right-clicking on the file we want to see the history of.
+
+
+# I also can see the structure.
+# Structure breaks down the code to all the variables and the function.
+# When you'll start having lots and lots of functions and variable structure function is handy.
+
+
+add(3, 4)
+
+# to change multiple function name you can refactor the name instead of doing it manually.
+
+print("addition is addition")
+# Refactor function only changes the names of the one selected function.
